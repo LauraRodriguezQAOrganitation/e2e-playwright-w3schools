758a7fb4c1a0c0a0556dd6f6df19d8ab6ef810a9
Laura Rodrigue <rgmarialaura@gmail.com>
2026-02-02 21:45:32 +0100

    test moviles menu sections

--- tests/test_menu_sections.py
+++ tests/test_menu_sections.py
@@ -1,69 +1,67 @@
 from playwright.sync_api import Page, expect
 import re
 
+#Aceptamos las cookies
+def accept_cookies_if_present(page: Page):
+    accept_button = page.get_by_role("button", name="Accept")
+    if accept_button.is_visible():
+        accept_button.click()
+
+#Abrimos el menu
+def open_menu(page: Page):
+    menu_button = page.locator("#navbtn_tutorials")
+
+    if menu_button.is_visible():
+        menu_button.click()
+    else:
+        page.get_by_role("button", name="Tutorials").hover()
+        page.locator("#nav_tutorials").wait_for(state="visible")
+
+
 def test_visit_menu_links(page: Page):
 
-    print("Given the user visits W3Schools homepage")
     page.goto("https://www.w3schools.com/")
-
-    print("When the page is loaded")
+    accept_cookies_if_present(page)
     page.wait_for_url("https://www.w3schools.com/")
 
     #HTML
-    print("And click on HTML menu")
-    page.get_by_role("link", name="HTML", exact=True).click()
-
-    print("Then user should be on HTML Tutorial page")
-    expect(page).to_have_url("https://www.w3schools.com/html/default.asp")
+    open_menu(page)
+    page.locator("#nav_tutorials").get_by_role("link", name="HTML", exact=True).click()
     expect(page).to_have_url(re.compile("html"))
     expect(page).to_have_title("HTML Tutorial")
-    expect(page.get_by_role("heading", name="HTML Tutorial", exact=True)).to_be_visible
+    expect(page.locator("h1")).to_be_visible()
 
     #CSS
-    print("And click on CSS menu")
-    page.get_by_role("link", name="CSS", exact=True).click()
-
-    print("Then user should be on CSS Tutorial page")
-    expect(page).to_have_url("https://www.w3schools.com/css/default.asp")
+    open_menu(page)
+    page.locator("#nav_tutorials").get_by_role("link", name="CSS", exact=True).click()
     expect(page).to_have_url(re.compile("css"))
     expect(page).to_have_title("CSS Tutorial")
-    expect(page.get_by_role("heading", name="CSS Tutorial", exact=True)).to_be_visible
+    expect(page.locator("h1")).to_be_visible()
 
     #JAVASCRIPT
-    print("And click on JAVASCRIPT menu")
-    page.get_by_role("link", name="JAVASCRIPT", exact=True).click()
-
-    print("Then user should be on JavaScript Tutorial page")
-    expect(page).to_have_url("https://www.w3schools.com/js/default.asp")
+    open_menu(page)
+    page.locator("#nav_tutorials").get_by_role("link", name="JAVASCRIPT", exact=True).click()
     expect(page).to_have_url(re.compile("js"))
     expect(page).to_have_title("JavaScript Tutorial")
-    expect(page.get_by_role("heading", name="JavaScript Tutorial", exact=True)).to_be_visible
+    expect(page.locator("h1")).to_be_visible()
 
     #SQL
-    print("And click on SQL menu")
-    page.get_by_role("link", name="SQL", exact=True).click()
-
-    print("Then user should be on SQL Tutorial page")
-    expect(page).to_have_url("https://www.w3schools.com/sql/default.asp")
+    open_menu(page)
+    page.locator("#nav_tutorials").get_by_role("link", name="SQL", exact=True).click()
     expect(page).to_have_url(re.compile("sql"))
     expect(page).to_have_title("SQL Tutorial")
-    expect(page.get_by_role("heading", name="SQL Tutorial", exact=True)).to_be_visible
+    expect(page.locator("h1")).to_be_visible()
 
     #PYTHON
-    print("And click on PYTHON menu")
-    page.get_by_role("link", name="PYTHON", exact=True).click()
-
-    print("Then user should be on Python Tutorial page")
-    expect(page).to_have_url("https://www.w3schools.com/python/default.asp")
+    open_menu(page)
+    page.locator("#nav_tutorials").get_by_role("link", name="PYTHON", exact=True).click()
     expect(page).to_have_url(re.compile("python"))
     expect(page).to_have_title("Python Tutorial")
-    expect(page.get_by_role("heading", name="Python Tutorial", exact=True)).to_be_visible
+    expect(page.locator("h1")).to_be_visible()
 
     #JAVA
-    print("And click on JAVA menu")
-    page.get_by_role("link", name="JAVA", exact=True).click()
-
-    print("Then the user should be on Java Tutorial page")
-    expect(page).to_have_url("https://www.w3schools.com/java/default.asp")
+    open_menu(page)
+    page.locator("#nav_tutorials").get_by_role("link", name="JAVA", exact=True).click()
+    expect(page).to_have_url(re.compile("java"))
     expect(page).to_have_title("Java Tutorial")
-    expect(page.get_by_role("heading", name=" Tutorial", exact=True)).to_be_visible
+    expect(page.locator("h1")).to_be_visible()
