b6b1bf9f75e96be64f76e32ae74168d9cf2a22c9
Laura Rodrigue <rgmarialaura@gmail.com>
2026-02-02 22:17:00 +0100

    Update test_negative_search.py

--- tests/test_negative_search.py
+++ tests/test_negative_search.py
@@ -1,33 +1,19 @@
 from playwright.sync_api import Page, expect
+from utils import accept_cookies_if_present, open_search_if_mobile
 
-#Aceptamos las cookies
-def accept_cookies_if_present(page: Page):
-    try:
-        accept_button = page.get_by_role("button", name="Accept")
-        if accept_button.is_visible():
-            accept_button.click()
-    except:
-        pass
-
-#Abrir buscador si es móvil
-def open_search_if_mobile(page: Page):
-    search_button = page.locator("#tnb-google-search-btn")
-    if search_button.count() > 0 and search_button.is_visible():
-        search_button.click()
-
-#Buscamos una variable inexistente (QA Manual)
 def test_search_no_results_for_qa_manual(page: Page):
 
     #Dado que el usuario se encuentra en la web
     page.goto("https://www.w3schools.com")
 
-    #(CLAVE en test móvil)
+    #(CLAVE en prueba de escritorio y movil)
     accept_cookies_if_present(page)
     open_search_if_mobile(page)
 
-    #Cuando busca una variable que no existe
+    #Cuando busca una variable que no existe (Como QA Manual)
     search_input = page.get_by_placeholder("Search...")
-    search_input.wait_for(state="visible")
+    expect(search_input).to_be_visible()
+
     search_input.fill("QA Manual")
     search_input.press("Enter")
 
