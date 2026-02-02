63d8044f1c432004493b88a8876a4bca6a4e354a
Laura Rodrigue <rgmarialaura@gmail.com>
2026-02-02 23:15:59 +0100

    test desktop negative search

--- tests/test_negative_search.py
+++ tests/test_negative_search.py
@@ -3,22 +3,22 @@ from utils import accept_cookies_if_present, open_search_if_mobile
 
 def test_search_no_results_for_qa_manual(page: Page):
 
-    #Dado que el usuario se encuentra en la web
+#Dado que el usuario se encuentra en la web
     page.goto("https://www.w3schools.com")
 
-    #(CLAVE en prueba de escritorio y movil)
+#(CLAVE en prueba de escritorio y movil)
     accept_cookies_if_present(page)
     open_search_if_mobile(page)
 
-    #Cuando busca una variable que no existe (Como QA Manual)
-    search_input = page.get_by_placeholder("Search...")
+#Cuando busca una variable que no existe (Como QA Manual)
+    search_input = page.locator("#tnb-google-search-input")
     expect(search_input).to_be_visible()
 
     search_input.fill("QA Manual")
     search_input.press("Enter")
 
-    #Entonces no se muestra contenido relacionado
+#Entonces no se muestra contenido relacionado
     expect(page.get_by_text("QA Manual Tutorial")).not_to_be_visible()
 
-    #Y la página sigue funcionando correctamente
+#Y la página sigue funcionando correctamente
     expect(page.locator("body")).to_be_visible()
